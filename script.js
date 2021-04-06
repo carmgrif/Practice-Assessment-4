85b423b5888a9497421170c7f6e426637c3c8353
carmgrif <carmgrif@umich.edu>
2021-04-06 00:19:02 -0400

    adding tsconfig file and js file

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+const dealership = [
+    {
+        make: 'Toyota',
+        model: 'Prius',
+        year: 2020,
+        hybrid: true
+    },
+    {
+        make: 'Toyota',
+        model: 'Prius',
+        year: 2021,
+        hybrid: true
+    },
+    {
+        make: 'Ford',
+        model: 'F-150',
+        year: 2015,
+        hybrid: false
+    },
+    {
+        make: 'Ferrari',
+        model: 'Enzo',
+        year: 2004,
+        hybrid: false
+    }
+];
+function findCars(arrayOfCars) {
+}
+function addCar() {
+}
+function areThereAnyHybridsForSale() {
+}
+function expiredWarranty() {
+}
+;
