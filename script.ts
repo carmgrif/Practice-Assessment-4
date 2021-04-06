9f3686cb122ae2a8da921fe6bd8274a689abfc98
carmgrif <carmgrif@umich.edu>
2021-04-06 00:09:07 -0400

    setup baseline typescript file

---
+++ script.ts
@@ -0,0 +1,53 @@
+
+interface Car[]{
+    make: 'string';
+    model: 'string';
+    year: 'number';
+    hybrid: 'boolean';
+}
+
+let dealership = dealership: Car[]{
+    {
+    make: 'Toyota',
+    model: 'Prius',
+    year: 2020,
+    hybrid: true;
+    },
+
+    {
+    make: 'Toyota',
+    model: 'Prius',
+    year: 2021,
+    hybrid: true
+    },
+
+    {
+    make: 'Ford',
+    model: 'F-150',
+    year: 2015,
+    hybrid: false
+    },
+
+    {
+    make: 'Ferrari',
+    model: 'Enzo',
+    year: 2004,
+    hybrid: false
+    }
+}
+
+function findCars(arrayOfCars) {
+    
+}
+
+function addCar() {
+    
+}
+
+function areThereAnyHybridsForSale() {
+    
+}
+
+function expiredWarranty() {
+    
+};
